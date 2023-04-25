1b178bc5f7270fcc19513b79a83cd1f9d30ec7f6
SavyonMalko <124379798+SavyonMalko@users.noreply.github.com>
2023-04-25 20:12:15 +0000

    done html

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// Copyright (c) 2022 Ms Raffin All rights reserved
+//
+// Created by: Ms Raffin
+// Created on: March 2022
+// This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function displays the discount depending on age and day of the week.
+ */
+function displayCost() {
+  	// initialize variables
+	let cost = "Please make a selection above."
+	
+	// get age and day of the week
+	let age = parseInt(document.getElementById('age').value)	
+	let select = document.getElementById('day');
+	let day = select.options[select.selectedIndex].value;
+
+  	if (age < 5 == 1) {
+		cost = "*****"
+	}
+	else if (age < 5 || age > 95) {
+		cost = "The cost is FREE for you."
+	}
+	else if ((day == "Tuesday") || (day == "Thursday)") 
+			 || (age >=12) && (age <=21)) {
+		cost = "You get a student discount."
+	}
+	else if ((age > 0) || (day != "")) {		
+		cost = "You have to pay regular price."
+	}
+	
+  	// display the greeting
+  	document.getElementById('display-results').innerHTML = cost
+}
