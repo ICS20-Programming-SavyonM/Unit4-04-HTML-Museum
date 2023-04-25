d96ceaf93bb16e4ebfe6c2044a470ed8aa5d5c14
SavyonMalko <124379798+SavyonMalko@users.noreply.github.com>
2023-04-25 21:38:37 +0000

    updated

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-// Copyright (c) 2022 Ms Raffin All rights reserved
+// Copyright (c) 2022 Savyon All rights reserved
 //
-// Created by: Ms Raffin
-// Created on: March 2022
+// Created by: Savyon
+// Created on: April 2023
 // This file contains the JS functions for index.html
 
 "use strict"
@@ -10,28 +10,40 @@
  * This function displays the discount depending on age and day of the week.
  */
 function displayCost() {
-  	// initialize variables
-	let cost = "Please make a selection above."
-	
-	// get age and day of the week
-	let age = parseInt(document.getElementById('age').value)	
-	let select = document.getElementById('day');
-	let day = select.options[select.selectedIndex].value;
+  // initialize variables
+  let cost = "Please enter all the values above.";
+  let errorMessage = "";
 
-  	if (age < 5 == 1) {
-		cost = "*****"
-	}
-	else if (age < 5 || age > 95) {
-		cost = "The cost is FREE for you."
-	}
-	else if ((day == "Tuesday") || (day == "Thursday)") 
-			 || (age >=12) && (age <=21)) {
-		cost = "You get a student discount."
-	}
-	else if ((age > 0) || (day != "")) {		
-		cost = "You have to pay regular price."
-	}
-	
-  	// display the greeting
-  	document.getElementById('display-results').innerHTML = cost
-}
+  // get age and day of the week
+  let age = parseInt(document.getElementById("age").value);
+  let select = document.getElementById("day");
+  let day = select.options[select.selectedIndex].value;
+
+  if (!age) {
+    errorMessage += "Please enter your age.<br>";
+  }
+  if (day === "") {
+    errorMessage += "Please select a day of the week.<br>";
+  }
+  if (errorMessage !== "") {
+    document.getElementById("error-message").innerHTML = errorMessage;
+  } else {
+    document.getElementById("error-message").innerHTML = "";
+  }
+
+  if (age < 5) {
+    cost = "You get in for FREE!";
+  } else if (age >= 5 && age <= 95) {
+    if (day === "Tuesday" || day === "Thursday") {
+      cost = "You get a student discount!";
+      if (age < 0) {
+        cost = "Please enter a valid age."
+    } else {
+      cost = "You have to pay regular price.";
+    }
+  }
+
+  // display the result
+  document.getElementById("display-results").innerHTML = cost;
+} 
+  }
